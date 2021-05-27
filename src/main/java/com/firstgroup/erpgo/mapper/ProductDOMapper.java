a65997b7a34fa342b81ed90e421454c912e21be1
不爱吃鱼的猫丶 <1716224950@qq.com>
2021-05-27 23:33:29 +0800

    商品库存信息添加开发ing

---
+++ src/main/java/com/firstgroup/erpgo/mapper/ProductDOMapper.java
@@ -0,0 +1,10 @@
+package com.firstgroup.erpgo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.firstgroup.erpgo.model.DO.ProductsDO;
+import com.firstgroup.erpgo.model.DO.SystemUserDO;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductDOMapper extends BaseMapper<ProductsDO> {
+}
